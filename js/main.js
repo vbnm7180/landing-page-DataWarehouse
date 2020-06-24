f178bb9f566db4fd454e57c7445ab563054d3799
vbnm7180 <vbnm7180@mail.ru>
2020-06-24 14:10:28 +0400

    master

---
+++ js/main.js
@@ -0,0 +1,9 @@
+$('.f-about__header').click(function() {
+    $('.f-about__links-block').slideToggle();
+});
+$('.f-help__header').click(function() {
+    $('.f-help__links-block').slideToggle();
+});
+$('.f-social__header').click(function() {
+    $('.f-social__links-block').slideToggle();
+});
